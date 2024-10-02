15ad43811a4a7691f48824834e95538b70b37804
YSLee-Dev <ysleedev01@gmail.com>
2024-10-02 17:28:38 +0900

    Feat(Detail): 뒤로가기 버튼, 제스처 비활성화

--- lib/Detail/Detail.dart
+++ lib/Detail/Detail.dart
@@ -13,9 +13,17 @@ class Detail extends StatefulWidget {
 class _DetailState extends State<Detail> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Detail Widget"), backgroundColor: this.widget._nowShowingTitleColor,),
-      body: Text("Welcome to Detail Widget"),
+    return PopScope(
+        canPop: false,
+        child: Scaffold(
+          appBar: AppBar(title: Text("Detail Widget"), backgroundColor: this.widget._nowShowingTitleColor,),
+          body: Row(
+            children: [
+              Text("Home에서 사용하고 싶은 색상을 선택하세요.")
+              
+            ],
+          ),
+        )
     );
   }
 }
