4aa16da0885350bed49a212d0e32147cc03ea1a7
YSLee-Dev <ysleedev01@gmail.com>
2024-10-04 11:19:34 +0900

    Study(Detail): 특정 위젯에 대한 설명(공부) 추가

--- lib/Detail/Detail.dart
+++ lib/Detail/Detail.dart
@@ -27,6 +27,9 @@ class Detail extends StatefulWidget {
 class _DetailState extends State<Detail> {
   @override
   Widget build(BuildContext context) {
+    // PopScope를 사용하여 제스처, 버튼을 통한 Navigator.pop()을 방지할 수 있음
+    // canPop에 가능 여부를 넣으며, onPopInvokedWithResult(){}를 통해 이벤트 여부를 받을 수 있음
+    // AOS 14 이상 버전부터 제공되는 추가 기능으로 인해 WillPopScope는 사용하지 않게 됨
     return PopScope(
         canPop: false,
         child: Scaffold(
@@ -44,11 +47,14 @@ class _DetailState extends State<Detail> {
                 height: 30,
               ),
               Expanded(
+                // List에 있는 값을 이용하여 여러개의 Widget과 스크롤 가능한 Widget을 만드는 Widget
                   child: ListView.builder(
                       itemCount: this.widget.colors.length,
                       itemBuilder: (BuildContext context, int index) {
+                        // 다양한 제스처를 받을 수 있는 Widget
                         return GestureDetector(
                           onTap: () {
+                            // pop을 하면서 데이터를 전달할 경우 push를 했던 부모에게 데이터가 전달됨
                             Navigator.pop(context, this.widget.colors[index],);
                           },
                           child: Container(
